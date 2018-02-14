8f632ba7671e64dd54532dd9091080f4c5374931
vaubarth <vdbarth@posteo.at>
2018-02-14 06:40:46 +0100

    Version 1.0.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name='anypath',
-    version='0.9.0',
+    version='1.0.0',
     description='AnyPath makes it trivial to fetch remote resources and work with them locally.',
     long_description=readme(),
     url='https://github.com/vaubarth/anypath',
